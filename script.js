1a178530c404d50ca295e2e8e1142b1b4fc10775
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2022-08-19 23:57:57 +0430

    make randomly array of objects

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const cards_array = [
     {
-        name:''
-    }
+        name:'shape1',
+        img : 'images/shape1.png'
+    },
+    {
+        name: 'shape2',
+        img: 'images/shape2.png'
+    },
+    {
+        name: 'shape3',
+        img: 'images/shape3.png'
+    },
+    {
+        name: 'shape4',
+        img: 'images/shape4.png'
+    },
+    {
+        name: 'shape5',
+        img: 'images/shape5.png'
+    },
+    {
+        name: 'shape6',
+        img: 'images/shape6.png'
+    },
 ]
+cards_array.sort(() => 0.5 - Math.random() );
+console.log(cards_array);
+
